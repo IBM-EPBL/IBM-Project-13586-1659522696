a3358db0558c5527d3518ee3174ceb8c0005ff10
R Nanda Kumar <jerrynandak@gmail.com>
2022-11-25 19:54:27 +0530

    sprint 2 android app

---
+++ Project Development Phase/Sprint 2/COVID Tracker/app/src/main/java/com/example/geofencing/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.geofencing;
+
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button btn = (Button)findViewById(R.id.buttonLogin);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MapsActivity.class));
+            }
+        });
+    }
+}
